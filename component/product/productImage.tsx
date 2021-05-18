1570924e4eab87b9eb5b065933bd6c68f884454e
Rahul Kumar <58592540+isrkrahul53@users.noreply.github.com>
2021-05-18 22:24:16 +0530

    Update productImage.tsx

--- component/product/productImage.tsx
+++ component/product/productImage.tsx
@@ -7,10 +7,10 @@ export default function ProductImageBanner(props){
 
     
     return <div className="overflow-hidden">
-        <img src={props.images[selected].src} className="w-full" alt="" />
+        <Link href={props.images[selected].href}><img src={props.images[selected].src} className="w-full" alt="" /></Link>
         <div className="text-right flex items-center justify-end -mt-8 px-2">
-            <Link href={props.images[selected].href}><img onClick={e=>selected > 0 && setSelected(selected-1)} src="/assets/icons/prev.png" width="20px" className="bg-gray-50 m-1 p-1 rounded hover:scale-105" alt="prev" /></Link>
-            <Link href={props.images[selected].href}><img onClick={e=>selected < props.images.length-1 && setSelected(selected+1)} src="/assets/icons/next.png" width="20px" className="bg-gray-50 m-1 p-1 rounded hover:scale-105" alt="next" /></Link>
+            <img onClick={e=>selected > 0 && setSelected(selected-1)} src="/assets/icons/prev.png" width="20px" className="bg-gray-50 m-1 p-1 rounded hover:scale-105" alt="prev" />
+            <img onClick={e=>selected < props.images.length-1 && setSelected(selected+1)} src="/assets/icons/next.png" width="20px" className="bg-gray-50 m-1 p-1 rounded hover:scale-105" alt="next" />
         </div>
     </div>
 }
